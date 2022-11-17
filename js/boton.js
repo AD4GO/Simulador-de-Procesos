97d25e10a077eec94c9d9dafb5d1468375b692a7
AD4GO <anuargomes1536@gmail.com>
2022-11-17 15:26:40 -0500

    4to cambio

--- js/boton.js
+++ js/boton.js
@@ -16,4 +16,22 @@ btn.addEventListener('click', () => {
     }
 })
 
+const ctx = document.getElementById('myChart')
+const procesos = ['p1','p2','p3', 'p4', 'p5', 'p6']
+const turnRound = [2,1,6,4,5,3]
+Chart.defaults.color = '#fff';
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels : procesos,
+        datasets: [{
+            label: '',
+            data: turnRound,
+            backgroundClor: [
+                '#111'
+            ],
+        }]
+    }
+})
+
 
