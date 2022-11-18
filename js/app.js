e254078926352af7b82f12ced8d2e738abae71e2
Bryan-JR <95584508+Bryan-JR@users.noreply.github.com>
2022-11-18 17:11:58 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -15,23 +15,3 @@ btn.addEventListener('click', () => {
         play.classList.add("active");
     }
 })
-
-const ctx = document.getElementById('myChart')
-const procesos = ['p1','p2','p3', 'p4', 'p5', 'p6']
-const turnRound = [2,1,6,4,5,3]
-Chart.defaults.color = '#fff';
-const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels : procesos,
-        datasets: [{
-            label: '',
-            data: turnRound,
-            backgroundClor: [
-                '#111'
-            ],
-        }]
-    }
-})
-
-
--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const simulador = {
             <div class="BotonEmpezar">
             <div class="InputQuantum">
                 <div class="input-data">
+                <input type="number" required class="inpQuantum" v-model="th"/>
+                <label>TH (ms)</label>
+                </div>
+            </div>
+            <div class="InputQuantum">
+                <div class="input-data">
                 <input type="number" required class="inpQuantum" v-model="quantum"/>
-                <label>QUANTUM</label>
+                <label>QUANTUM (ms)</label>
                 </div>
             </div>
             <div class="container" v-on:click="accionBoton">
@@ -44,7 +50,7 @@ const simulador = {
                         <td>{{pr.pid}}</td>
                         <td>{{pr.nombre}}</td>
                         <td>{{pr.prioridad}}</td>
-                        <td>{{pr.nombre.length}}</td>
+                        <td>{{(pr.nombre.length * th)/1000}}</td>
                     </tr>
                 </tbody>
                 </table>
@@ -57,7 +63,7 @@ const simulador = {
                 <h3>Proceso en Ejecucion</h3>
                 <li>PID = {{proceso.pid}}</li>
                 <li>nombre = {{proceso.nombre}}</li>
-                <li>Rafaga = {{ proceso.nombre.length }}</li>
+                <li>Rafaga = {{(proceso.nombre.length * th)/1000}}</li>
                 <li>Tiempo Restante = {{proceso.restante}}</li>
                 </ul>
             </div>
@@ -84,12 +90,87 @@ const simulador = {
             </div>
             </section>
         </div>
+        <h2>Reportes</h2>
+    <div class="contenedor2">
+      <section class="estadoProceso">
+        <div class="ListEjecucion">
+          <h3 class="tituloTabla">Listado de ejecución</h3>
+          <table class="tablaProcesos" id="tableTerminado">
+            <thead>
+              <tr class="headertable">
+                <th>P</th>
+                <th>TL</th>
+                <th>R</th>
+                <th>P.R</th>
+                <th>T.R</th>
+                <th>T.F</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="rp, i in finalizados":key="i">
+                <td>{{rp.nombre}}</td>
+                <td>{{rp.tiempoLlegada}}</td>
+                <td>{{(th*rp.nombre.length)/1000}}</td>
+                <td>{{rp.prioridad}}</td>
+                <td>{{rp.tiempoFin-rp.tiempoLlegada}}</td>
+                <td>{{rp.tiempoFin}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+      <section class="estadoProceso">
+        <div class="NoExpuls">
+          <h3 class="tituloTabla">Listado de procesos (no expulsivos)</h3>
+          <table class="tablaProcesos" id="tableTerminado">
+            <thead>
+              <tr class="headertable">
+                <th>P</th>
+                <th>TL</th>
+                <th>R</th>
+                <th>P.R</th>
+                <th>T.R</th>
+                <th>T.F</th>
+              </tr>
+            </thead>
+            <tbody>
+            <tr v-for="rp, i in finalizados":key="i" v-if="rp.prioridad=='0'">
+                <td>{{rp.nombre}}</td>
+                <td>{{rp.tiempoLlegada}}</td>
+                <td>{{(th*rp.nombre.length)/1000}}</td>
+                <td>{{rp.prioridad}}</td>
+                <td>{{rp.tiempoFin-rp.tiempoLlegada}}</td>
+                <td>{{rp.tiempoFin}}</td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </div>
+    <div class="graficaList">
+      <ul class="definiciones">
+        <li>P: Nombre proceso</li>
+        <li>T.L: Timepo de llegada</li>
+        <li>R: Rafaga</li>
+        <li>P.R: Prioridad asignada</li>
+        <li>T.R: TurnaRound</li>
+        <li>T.F: Tiempo de finalizacón</li>
+      </ul>
+      <section class="content">
+        <h2>Grafico de comportamiento</h2>
+        <h3 class="tituloGrafica">Proceso vs TurnaRound</h3>
+        <canvas id="myChart" height="100" width="200"> </canvas>
+      </section>
+    </div>
     </div>`,
   data() {
     return {
         boton: true,
         quantum: 0,
         tiempo: 0,
+        cont: 0,
+        th: 0,
         procesos: [],
         listos: [],
         finalizados: [],
@@ -102,7 +183,8 @@ const simulador = {
             tiempoLlegada: -1,
             tiempoFin: 0,
             rafaga: 0,
-            restante: 0
+            restante: 0,
+            iteraciones: 0
         },
     }
   },
@@ -117,60 +199,104 @@ const simulador = {
         }))
     },
     simulacionNoExpulsivos(){
-        this.proceso.pid = this.listos[0].pid;
-        this.proceso.nombre = this.listos[0].nombre;
-        this.proceso.rafaga = this.listos[0].nombre.length;
-        this.proceso.restante = this.proceso.rafaga;
-        this.listos[0].tiempoLlegada = this.tiempo;
-        let hilo = setInterval(()=>{
-            if(this.proceso.restante>0){
-                this.proceso.restante--;
-            } else {
-                this.listos[0].tiempoFin = this.tiempo;
-                this.finalizados.push(this.listos[0]);
-                this.listos.splice(0,1);
-                clearInterval(hilo);
-            }
-            this.tiempo++;
-        }, 1000);
+        if(this.listos[0].restante>0){
+            this.listos[0].restante--;
+        } else {
+            this.listos[0].tiempoFin = this.tiempo;
+            this.listos[0].iteraciones++;
+            this.finalizados.push(this.listos[0]);
+            this.listos.splice(0,1);
+            this.proceso.pid = this.listos[0].pid;
+            this.proceso.nombre = this.listos[0].nombre;
+            this.proceso.rafaga = (this.listos[0].nombre.length * this.th)/1000;
+            if(this.listos[0].restante==0)
+                this.listos[0].restante = this.proceso.rafaga;
+        }
+        this.proceso.restante = this.listos[0].restante;
+        this.tiempo++;
     },
     simulacionExpulsivos(){
-        this.proceso.pid = this.listos[0].pid;
-        this.proceso.nombre = this.listos[0].nombre;
-        this.proceso.rafaga = this.listos[0].nombre.length;
-        this.proceso.restante = this.proceso.rafaga;
-        this.listos[0].tiempoLlegada = this.tiempo;
-        let cont = 0;
-        let hilo = setInterval(() => {
-            if (cont<this.quantum && this.proceso.restante>0){
-                this.proceso.restante--;
-                cont++;
-            } else if (cont==this.quantum && this.proceso.restante>0){
-                this.listos.push(this.listos[0]);
-                this.listos.splice(0,1);
-                cont=0;
-                clearInterval(hilo);
-            } else {
-                this.listos[0].tiempoFin = this.tiempo;
-                this.finalizados.push(this.listos[0]);
-                this.listos.splice(0,1);
-                clearInterval(hilo);
-            }
-            this.tiempo++;
-        }, 1000);
+        if (this.cont<(this.quantum/1000) && this.listos[0].restante>0){
+            this.listos[0].restante--;
+            this.cont++;
+        } else if (this.cont==(this.quantum/1000) && this.listos[0].restante>0){
+            this.listos[0].iteraciones++;
+            this.listos.push(this.listos[0]);
+            this.listos.splice(0,1);
+            this.cont=0;
+            this.proceso.pid = this.listos[0].pid;
+            this.proceso.nombre = this.listos[0].nombre;
+            this.proceso.rafaga = (this.listos[0].nombre.length * this.th)/1000;
+            if(this.listos[0].restante==0)
+                this.listos[0].restante = this.proceso.rafaga;
+        } else {
+            this.listos[0].iteraciones++;
+            this.listos[0].tiempoFin = this.tiempo;
+            this.finalizados.push(this.listos[0]);
+            this.listos.splice(0,1);
+            this.proceso.pid = this.listos[0].pid;
+            this.proceso.nombre = this.listos[0].nombre;
+            this.proceso.rafaga = (this.listos[0].nombre.length * this.th)/1000;
+            if(this.listos[0].restante==0)
+                this.listos[0].restante = this.proceso.rafaga;
+        }
+        this.proceso.restante = this.listos[0].restante;
+        this.tiempo++;
     }, 
     async accionBoton(){
+        var interval
         if(this.boton){
             this.boton = false;
-            this.comenzar()
+            interval = setInterval(()=>{
+                //Para el primer proceso
+                if (this.tiempo == 0 ){
+                    this.proceso.pid = this.listos[0].pid;
+                    this.proceso.nombre = this.listos[0].nombre;
+                    this.proceso.rafaga = (this.listos[0].nombre.length * this.th)/1000;
+                    this.listos[0].restante = this.proceso.rafaga;
+                    this.proceso.restante = this.listos[0].restante;
+                    this.listos[0].tiempoLlegada = this.tiempo;
+                } else if (this.listos[0].tiempoLlegada==-1)
+                    this.listos[0].tiempoLlegada = this.tiempo;
+                if(this.listos[0].prioridad=="1") this.simulacionNoExpulsivos();
+                else this.simulacionExpulsivos();
+
+                if(this.listos.length==0 || this.boton){
+                    clearInterval(interval);
+                }
+            }, 100);
         } else {
             this.boton = true;
         }
-    },
-    async comenzar(){
-        if(this.listos[0].prioridad=="1") this.simulacionNoExpulsivos();
-        else this.simulacionExpulsivos();
+        if(this.listos.length==0){
+            this.graficar();
+        }
     }
+  },
+  graficar(){
+    const procesos = []
+    const turnRound = []
+    for (const process of this.finalizados) {
+        procesos.push(process.nombre);
+        turnRound.push(process.tiempoFin-process.tiempoLlegada);
+    }
+    console.log(procesos);
+    console.log(turnRound);
+    const ctx = document.getElementById('myChart');
+    Chart.defaults.color = '#fff';
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels : procesos,
+            datasets: [{
+                label: '',
+                data: turnRound,
+                backgroundClor: [
+                    '#111'
+                ],
+            }]
+        }
+    })
   },
   mounted() {
     this.capturarProcesos()
