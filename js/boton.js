936ea337ceac20f9db3007c4a5decc45753b694a
AD4GO <anuargomes1536@gmail.com>
2022-11-17 21:58:16 -0500

    cambio 5

--- js/boton.js
+++ js/boton.js
@@ -20,6 +20,7 @@ const ctx = document.getElementById('myChart')
 const procesos = ['p1','p2','p3', 'p4', 'p5', 'p6']
 const turnRound = [2,1,6,4,5,3]
 Chart.defaults.color = '#fff';
+Chart.defaults.size = 29;
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
